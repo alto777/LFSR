a9afdb95068c5bc4b3f2120d90c1c5481f7740e9
Robert A Moeser <ram32768@gmail.com>
2018-03-24 09:11:06 -0400

    This is my fourth try to do my 3rd first commit\!

---
+++ src/LFSR.hpp
@@ -0,0 +1,20 @@
+#include "rack.hpp"
+
+
+using namespace rack;
+
+
+extern Plugin *plugin;
+
+////////////////////
+// module widgets
+////////////////////
+
+//struct FG16Widget : ModuleWidget {
+//	FG16Widget();
+//};
+
+struct FG8Widget : ModuleWidget {
+	FG8Widget();
+//	Menu *createContextMenu() override;
+};
